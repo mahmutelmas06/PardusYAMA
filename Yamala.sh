01f4fca70aeeec2a782162e059904b43449d1feb
mahmutelmas06 <mahmutelmas06@hotmail.com>
2020-06-30 20:51:49 +0300

    Xfce ve Gnome uyumluluğu artırıldı

--- Yamala.sh
+++ Yamala.sh
@@ -110,9 +110,14 @@ _dir="/home/${u}"
 #==============================================================================			# Masaüstü türünü belirle
 
 
-#desktop=$(echo "$XDG_DATA_DIRS" | sed 's/.*\(xfce\|kde\|gnome\).*/\1/')
-desktop=$(echo "$XDG_CURRENT_DESKTOP")  											
-
+if [ "$XDG_CURRENT_DESKTOP" = "" ]
+then
+  desktop=$(echo "$XDG_DATA_DIRS" | sed 's/.*\(xfce\|kde\|gnome\).*/\1/')
+else
+  desktop=$XDG_CURRENT_DESKTOP
+fi
+desktop=${desktop,,}  # tüm harfler küçük	
+				
 
 #==============================================================================	    	
 
@@ -166,12 +171,11 @@ zenity 	--progress \
 		--width 400 \
 		--pulsate \
 		--auto-close
-
 #==============================================================================  Yükleme seçenekleri
 												  			
 
 
-if [ $(pgrep -c xfce4-panel) -gt 0 ]; then
+if [ "$desktop" = "xfce" ]; then
 
 action=$(zenity --list --checklist \
 	--height 500 --width 1000 \
@@ -188,7 +192,7 @@ action=$(zenity --list --checklist \
 			--separator=":")
 
 
-elif [ $(pgrep -c gnome-panel) -gt 0 ]; then
+if [ "$desktop" = "gnome" ]; then
 
 action=$(zenity --list --checklist \
 	--height 500 --width 975 \
@@ -234,7 +238,7 @@ apt-get -y remove gdebi						# Pardus Paket Yükleyici adı altında bir Gdebi k
 apt-get -y remove gimp              		# Son kullanıcı için Pinta zaten yüklü. İhtiyaç duyan grafikçiler Gimp yükleyebilir.
 apt-get -y remove vlc						# Totem var, bağımlılıklarından dolayı totem kaldırılamıyor.
 
-if [ $(pgrep -c gnome-panel) -gt 0 ]; then
+if [ "$desktop" = "gnome" ]; then
 apt-get -y remove synaptic					# Gnome paketler ile aynı paketleri listeliyor. Gnome paketler bağımlılıktan ve güncelleme yardımcısından dolayı kaldırılamıyor.
 
 apt-get -y install chrome-gnome-shell		# Gnome eklentileri tarayıcı eklentisini yükle
@@ -254,7 +258,7 @@ apt-get -y install ffmpeg 					# Video ve resim indirme ve düzenleme programlar
 
 echo "# Bazı yazılımlar Flatpak sürümleri ile değiştirilip güncelleştiriliyor.\n \nBu işlem internet hızınıza göre biraz zaman alabilir." ; sleep 2
 
-if [ $(pgrep -c xfce4-panel) -gt 0 ]; then
+if [ "$desktop" = "xfce" ]; then
 apt-get -y remove deepin-deb-installer thunderbird xfce4-dict xfburn
 
 flatpak install -y flathub org.gnome.Totem	# Gnome sürümünde Totem var bunda da olsun benzer olsun.
@@ -358,7 +362,7 @@ echo "# Samba kurulup kullanıma hazır hale gelmesi için ayarları yapılıyor
 
 apt-get -y install samba smbclient winbind libpam-winbind libnss-winbind samba-vfs-modules samba-common libcups2 cups cifs-utils
 
-if [ $(pgrep -c gnome-panel) -gt 0 ]; then
+if [ "$desktop" = "gnome" ]; then
 apt-get -y install nautilus-share
 fi
 
@@ -418,7 +422,7 @@ _FILESS="./Şablonlar/*"
           for f in $_FILESB
    do
     
-    if [ $(pgrep -c gnome-panel) -gt 0 ]; then
+    if [ "$desktop" = "gnome" ]; then
 
        cp -r "${f}" "$_dir/${BET}" #  Betikleri kopyala
 
